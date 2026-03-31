7bade64ffe3f0ef0bf237b32aa20ac54ae5e4110
Kimdoheon <doheon8847@gmail.com>
2026-03-31 12:40:32 +0900

    어레이 2차수정

---
+++ array/char-array-2.c
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS   
+#include <stdio.h>
+int main(void) {
+
+
+    int i;
+    char fruits[3][20]; {//5행 10열(0~4행 0~9열)
+
+        for (i = 0; i < 3; i++) {
+            printf("과일 이름을 입력하시오: ");
+            scanf("%s", &fruits[i]);
+            printf("%d 번째 과일은 : %s \n", i, fruits[i]);
+        }
+
+        return 0;
+    }
