e08a64a670070a6d4784cac8861c79cc3e3ab6d2
Kimdoheon <doheon8847@gmail.com>
2026-04-02 12:50:16 +0900

    포인터 실습

---
+++ pointer/pointer-1.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+int main(void)
+{
+	int i = 10;
+	char c = 69;
+	float f = 12.3;
+	printf("i의 값: %d\n", i);
+	printf("c의 값: %c\n", c);
+	printf("f의 값: %f\n", f);
+
+	printf("i의 주소: %p\n", &i);
+	printf("c의 주소: %p\n", &c);
+	printf("f의 주소: %p\n", &f);
+	return 0;
+
+
+
+
+
+
+
+}
---
+++ pointer/pointer-2.c
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int i = 3000;
+	int* p = NULL;  // 포인터변수 선언
+
+	p = &i;     // i변수의 주소를 p 포인터 변수에 줌
+
+	printf("p=%p\n", p);
+	//%p : 주소를 출력하는 서식 문자
+	printf("&i=%p\n\n", &i);
+	printf("*p = %d\n", *p);
+	i = 4000;
+	printf("i=%d\n", i);
+	*p = 5000;
+	printf("*p = %d\n", *p);
+	
+	//주소에 저장된 내용에 접근하는 포인터
+	int x = 10, y = 20;
+	int* pi; 
+
+	pi = &x;
+	printf("pi=%p\n", pi);
+	printf("*pi=%d\n", *pi);
+
+	pi = &y;
+	printf("pi = %p\n", pi);
+	printf("*pi = &d\n", *pi);
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+
+}
